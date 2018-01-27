29ae3b0a57a6c5cee1650d2e01951f50a0a1dfc2
Bohdan SUPRUN <bsuprun@e2r6p4.unit.ua>
2018-01-27 20:28:35 +0200

    rofl

--- get_next_line.c
+++ get_next_line.c
@@ -1,132 +1,132 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsuprun <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/27 17:17:35 by bsuprun           #+#    #+#             */
+/*   Updated: 2018/01/27 20:28:14 by bsuprun          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
 
-int		ft_stringchr(char	*str, char	c)
+int				ft_ttt(char *str)
+{
+	if (str[0] == '\0')
+		return (1);
+	else
+		return (0);
+
+}
+int				ft_stringchr(char	*str)
 {
 	int		i;
 	
 	i = 0;
-	while (str[i] && str[i] != c)
+	while (str[i] && str[i] != '\n')
 		i++;
 	return (i);
 }
 
-int		get_next_line(const int fd, char **line)
+void			trim(t_list **lst, int len)
+{
+	char	*s;
+
+	s = ft_strsub((*lst)->content, len + 1, ft_strlen((*lst)->content));
+	ft_bzero((*lst)->content, ft_strlen((*lst)->content));
+	free((*lst)->content);
+	(*lst)->content = s;
+	//free(s);
+}
+
+int			mamkin_coder(t_list *lst, char **line)
+{
+	int			i;
+
+	i = ft_stringchr(lst->content);
+	*line = ft_strsub(lst->content, 0, i);
+	trim(&lst, i);
+	return (1);
+}
+
+int				get_next_line(const int fd, char **line)
 {
 	static t_list	*lst;
 	int				ret;
-	char			*buf;
-//	char			*buf1;
+	char			buf[BUFF_SIZE + 1];
+	char			*buf1;
 
-	if (fd < 0 || !line)
+	if (fd < 0 || !fd || !line)
 		return (-1);
-	buf = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1));
 	while (lst && (lst->content_size != (size_t)fd))
 		lst = lst->next;
 	if (!lst)
 	{
-//		printf("NOT LST\n");
 		lst = ft_lstnew("", 1);
 		lst->content_size = fd;
 	}
-
-/*	if (ft_strchr(lst->content, '\n'))
-	{
-		//printf("WE ARE IN FT_STRCHR\n");
-		//printf("LST->CONTENT:%s\n", lst->content);
-		*line = ft_strsub(lst->content, 0, ft_stringchr(lst->content, '\n'));
-	//	printf("*LINE:%s\n", *line);
-		lst->content = ft_strsub(lst->content, ft_stringchr(lst->content, '\n'), ft_strlen(lst->content) - ft_stringchr(lst->content, '\n'));
-	}
-	if (lst->content || ((ret = read(fd, buf, BUFF_SIZE) > 0)))
-		return (1);
-	if (!lst->content)
-		return (0);
-*/
 	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
-		if (ft_strchr(buf, '\n'))
-		{
-			*line = ft_strsub(buf, 0, ft_stringchr(buf, '\n'));
-			if (!lst->content)
-				lst->content = ft_strsub(buf, ft_stringchr(buf, '\n'), ft_strlen(buf) - ft_stringchr(buf, '\n'));
-			else
-				lst->content = ft_strjoin(lst->content, ft_strsub(buf, ft_stringchr(buf, '\n'), ft_strlen(buf) - ft_stringchr(buf, '\n')));
-			return (1);
-		}
-/*
-		else
-		{
-			buf1 = lst->content;
-			lst->content = ft_strjoin(buf, buf1);
-			ft_strdel(&buf1);
-			return (1);
-		}
-*/
-/*	
-		if (ft_strchr(buf, '\n'))
-		{
-			*line = ft_strsub(buf, 0, ft_stringchr(buf, '\n'));
-			lst->content = ft_strsub(buf, ft_stringchr(buf, '\n'), ft_strlen(buf) - ft_stringchr(buf, '\n'));
-			break ;
-			return (1);
-			*line = ft_strsub(buf, 0, ft_stringchr(buf, '\n'));
-		//	if (lst->content)
-		//		return (1);
-		//	else
-		//		return (0);
-		}
-*/
-		/*
-		else
-		{
-			buf1 = lst->content;
-			lst->content = ft_strjoin(buf1, buf);
-			ft_strdel(&buf1);
-			return (0);
-		}
-		*/
-		if (ret < 0 && !lst->content)
-			return (0);
-/*
 		buf1 = lst->content;
-		lst->content = ft_strjoin(lst->content, buf);
+		lst->content = ft_strjoin(buf1, buf);
 		ft_strdel(&buf1);
-		if (ft_strchr(lst->content, '\n'))
-		{
-			*line = ft_strsub(lst->content, 0, ft_strlen(lst->content) - ft_stringchr(lst->content, '\n'));
-			lst->content = ft_strsub(lst->content, ft_stringchr(lst->content, '\n'), ft_strlen(lst->content) - ft_strlen(*line));
-			ft_strdel(&buf);
-			return (1);
-		}
-*/
 	}
-	if (ret < 0 && !lst->content)
+	//if ((char*)lst->content == '\0')
+	//	 return (0);
+	if (ft_ttt(lst->content))
 		return (0);
-	return (-1);
+	if (!ft_ttt(lst->content))
+		mamkin_coder(lst, line);
+	return (1);
+	
 }
 
-
-int		main(void)
+/*
+int		main(int ac, char **av)
 {
 	char	*line;
-//	char	*test;	
-//	char	c;
 	int		fd;
+//	int		fd1;
+//	int		fd2;
 
 //	test = "abcd'\n'assjjs";
 //	c = '\n';
 	
 //	printf("CHAR POS: %i\n", ft_stringchr(test, c));
-	fd = open("test.txt", O_RDONLY);
+	fd = open(av[1], O_RDONLY);
+//	fd1 = open("test1.txt", O_RDONLY);
+//	fd2 = open("test2.txt", O_RDONLY);
+
 	int		i = 1;
+	while ((i = get_next_line(fd, &line) > 0))
+	{
+		printf("%s\n", line);
+	}
 
 	get_next_line(fd, &line);
 	printf("%s\n", line);
 	get_next_line(fd, &line);
 	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+
+//	get_next_line(fd1, &line);
+//	printf("%s\n", line);
+//	get_next_line(fd1, &line);
+//	printf("%s\n", line);
+
+//	get_next_line(fd2, &line);
+//	printf("%s\n", line);
+//	get_next_line(fd2, &line);
+//	printf("%s\n", line);
 
 	return (0);
 }
+*/

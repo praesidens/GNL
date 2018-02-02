0a45a4f0ff95666de22b9997619a9a590e386876
Bohdan SUPRUN <bsuprun@e1r1p14.unit.ua>
2018-02-02 18:19:33 +0200

    upd_test

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bsuprun <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 17:17:35 by bsuprun           #+#    #+#             */
-/*   Updated: 2018/02/01 21:30:29 by bsuprun          ###   ########.fr       */
+/*   Updated: 2018/02/02 18:19:07 by bsuprun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,43 +24,53 @@ int				ft_nl(char *str)
 	return (i);
 }
 
-int				ft_reader(int fd, t_list **lst, char **line)
+
+
+int				ft_reader(int fd, t_list **lst)
 {
 	char	buf[BUFF_SIZE + 1];
 	char	*buf1;
 	int		ret;
 
-	if (!(*lst)->C)
-		return (0);
 	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
 		buf1 = (*lst)->C;
-		(*lst)->C = ft_strjoin(buf1, buf);
+		(*lst)->C = ft_strjoin((*lst)->C, buf);
 		ft_strdel(&buf1);
 	}
-	if ((*lst)->C && ft_strchr((*lst)->C, '\n'))
+	printf("(*lst)->C: %s\n", (*lst)->C);
+	return (ret);
+}
+
+int				ft_splitter(t_list **lst, char **line)
+{
+	if (ft_strchr((*lst)->C, '\n'))
 	{
 		*line = ft_strsub((*lst)->C, 0, ft_nl((*lst)->C));
+		printf("LINE V IF:%s\n", *line);
 		(*lst)->C = ft_strsub((*lst)->C, ft_nl((*lst)->C) + 1,
-				ft_strlen((*lst)->C) - ft_nl((*lst)->C) - 1);
+			   	ft_strlen((*lst)->C - ft_nl((*lst)->C)));
+		printf("IF\n");
+		printf("(*lst)->C V IF: %s\n", (*lst)->C);
 		return (1);
 	}
-	else if (!ft_strchr((*lst)->C, '\n') && (*lst)->C)
+	else if (!ft_strchr((*lst)->C, '\n' && ft_strlen((*lst)->C) > 0))
 	{
+		printf("ELSE IF\n");
 		*line = (*lst)->C;
-		(*lst)->C = NULL;
+		(*lst)->C = ft_strsub((*lst)->C, 0, 0);
 		return (1);
 	}
 	else
 		return (0);
-}	
-		
+}
+
 
 int				get_next_line(const int fd, char **line)
 {
 	static t_list	*lst;
-	//int				ret;
+//	int				ret;
 
 	if (fd < 0 || !line)
 		return (-1);
@@ -68,10 +78,12 @@ int				get_next_line(const int fd, char **line)
 	//	lst = lst->next;
 	if (!lst && (lst = ft_lstnew("", 1)))
 		lst->C_S = fd;
-	return (ft_reader(fd, &lst, line));
+	if (ft_strlen(lst->C) < 1)
+		ft_reader(fd, &lst);
+	return (ft_splitter(&lst, line));
 }
 
-/*
+
 int		main(int ac, char **av)
 {
 	char	*line;
@@ -98,4 +110,4 @@ int		main(int ac, char **av)
 
 	return (0);
 }
-*/
+
